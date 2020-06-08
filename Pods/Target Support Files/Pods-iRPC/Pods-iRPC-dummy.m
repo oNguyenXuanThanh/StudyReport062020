feb02c6cefc4fa292bb33a5a899c55da07f6603b
Thanh Nguyen Xuan <nguyen.xuan.thanh@sun-asterisk.com>
2020-06-08 16:44:59 +0700

    Initial commit

---
+++ Pods/Target Support Files/Pods-iRPC/Pods-iRPC-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iRPC : NSObject
+@end
+@implementation PodsDummy_Pods_iRPC
+@end
---
+++ iRPC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iRPC
+//
+//  Created by Thanh Nguyen Xuan on 6/8/20.
+//  Copyright © 2020 Thanh Nguyen Xuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
